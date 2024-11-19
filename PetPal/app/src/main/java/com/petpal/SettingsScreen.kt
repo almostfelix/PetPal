9dfb097c8e6c09719dc4a7bfdde8c962a6112b78
nagyg <nagy.gergely244@gmail.com>
2024-11-19 20:37:52 +0100

    - Created 4 setting categories: Account, Notifications, Appearance, Privacy and Security. Each category now has a default page.

--- PetPal/app/src/main/java/com/petpal/SettingsScreen.kt
+++ PetPal/app/src/main/java/com/petpal/SettingsScreen.kt
@@ -1,10 +1,11 @@
-// SettingsScreen.kt
 package com.petpal
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -12,6 +13,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.petpal.ui.theme.JetpackComposeTestTheme
 
 @Composable
@@ -22,107 +24,134 @@ fun SettingsScreen(navController: NavController) {
             .background(color = colorResource(id = R.color.bg)),
         contentAlignment = Alignment.TopStart
     ) {
-
-        // Settings content
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-
-
-        )
-        {
+        ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-
-
-                // Back Button
-                IconButton(
-                    onClick = { navController.popBackStack() },
+                Card(
                     modifier = Modifier
-                        .size(40.dp)
+                        .width(40.dp)
+                        .height(40.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                    colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.bg)),
+                    shape = RoundedCornerShape(25.dp),
+                    onClick = { navController.popBackStack() }
                 ) {
-                    Icon(
-                        painter = painterResource(R.drawable.baseline_arrow_back_32),
-                        contentDescription = "Back",
-                        tint = colorResource(id = R.color.black_icon)
-                    )
-                }
-
-            }
-            SettingItem(title = "Dark Mode")
-        }
-    }
-
-}
-
-@Composable
-fun SettingItem(title: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = title,
-            modifier = Modifier.weight(1f),
-            style = MaterialTheme.typography.bodyLarge
-        )
-        Switch(checked = false, onCheckedChange = {})
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SettingsScreenPreview() {
-    JetpackComposeTestTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = colorResource(id = R.color.bg)),
-            contentAlignment = Alignment.TopStart
-        ) {
-
-            // Settings content
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-
-
-            )
-            {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Start,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-
-
-                    // Back Button
-                    IconButton(
-                        onClick = { },
+                    Box(
                         modifier = Modifier
-                            .size(40.dp)
+                            .width(40.dp)
+                            .height(40.dp)
+                            .fillMaxHeight(),
                     ) {
                         Icon(
                             painter = painterResource(R.drawable.baseline_arrow_back_32),
-                            contentDescription = "Back",
-                            tint = colorResource(id = R.color.black_icon)
+                            contentDescription = null,
+                            tint = colorResource(id = R.color.black_icon),
+                            modifier = Modifier
+                                .size(32.dp)
+                                .align(Alignment.Center)
                         )
                     }
 
                 }
-                SettingItem(title = "Dark Mode")
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+
+            ) {
+                SettingsCategory(
+                    title = "Account Information",
+                    icon = R.drawable.baseline_account_circle_32,
+                    onClick = { navController.navigate("settings/account_inf") }
+                )
+                HorizontalDivider(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    thickness = 1.dp,
+                    color = colorResource(id = R.color.shadow)
+                )
+
+                SettingsCategory(
+                    title = "Notifications",
+                    icon = R.drawable.baseline_circle_notifications_32,
+                    onClick = { navController.navigate("settings/notif") }
+                )
+                HorizontalDivider(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    thickness = 1.dp,
+                    color = colorResource(id = R.color.shadow)
+                )
+
+                SettingsCategory(
+                    title = "Appearance",
+                    icon = R.drawable.baseline_brush_32,
+                    onClick = { navController.navigate("settings/appear") }
+
+                )
+                HorizontalDivider(
+                    modifier = Modifier.padding(horizontal = 16.dp), thickness = 1.dp,
+                    color = colorResource(id = R.color.shadow)
+                )
+
+                SettingsCategory(
+                    title = "Privacy and Security",
+                    icon = R.drawable.baseline_gpp_good_32,
+                    onClick = { navController.navigate("setting/privacy_security") }
+                )
             }
         }
+    }
+}
+
+@Composable
+fun SettingsCategory(title: String, icon: Int, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(vertical = 8.dp),
+
+    ) {
+        Row(modifier = Modifier.align(Alignment.CenterStart)) {
+
+            Icon(
+            painter = painterResource(id = icon),
+            contentDescription = title,
+            modifier = Modifier.size(32.dp)
+        )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+        }
+
+        Icon(
+            painter = painterResource(R.drawable.baseline_arrow_forward_ios_32),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp).align(Alignment.CenterEnd))
+
+
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsScreenPreview() {
+    val navController = rememberNavController()
+    JetpackComposeTestTheme {
+        SettingsScreen(navController = navController)
     }
 }
